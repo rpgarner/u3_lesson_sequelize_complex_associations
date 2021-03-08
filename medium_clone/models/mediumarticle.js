03b551f664dbf88eff1e7857b235da7896a488f1
Andre Pato <anpato1994@gmail.com>
2021-03-08 13:04:53 -0500

    add queries

--- medium_clone/models/mediumarticle.js
+++ medium_clone/models/mediumarticle.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       MediumArticle.belongsTo(models.MediumUser, {
-        as: 'authored',
+        as: 'author',
         foreignKey: 'authorId'
       })
       MediumArticle.belongsToMany(models.MediumUser, {
---
+++ medium_clone/query.js
@@ -0,0 +1,57 @@
+const { MediumUser, MediumArticle, sequelize } = require('./models')
+const stringify = require('../utils')
+
+const userAndArticle = async () => {
+  try {
+    const data = await MediumUser.findAll({
+      include: [{ model: MediumArticle, as: 'authored' }]
+    })
+    stringify(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getArticleAuthors = async () => {
+  try {
+    const articles = await MediumArticle.findAll({
+      include: [
+        { model: MediumUser, as: 'author', attributes: ['id', 'username'] }
+      ]
+    })
+    stringify(articles)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getReadingList = async () => {
+  try {
+    const list = await MediumUser.findAll({
+      include: [
+        {
+          model: MediumArticle,
+          as: 'reading_list',
+          through: { attributes: [] }
+        }
+      ]
+    })
+    stringify(list)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function main() {
+  try {
+    // await userAndArticle()
+    await getArticleAuthors()
+    // await getReadingList()
+  } catch (error) {
+    console.log(error)
+  } finally {
+    sequelize.close()
+  }
+}
+
+main()
